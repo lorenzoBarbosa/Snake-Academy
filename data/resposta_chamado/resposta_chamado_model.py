35cd40ea5fa70c13cbe562e8748d2a9992759646
JhonatanLimaMotaLourenco <jhonatanlmlourenco@gmail.com>
2025-06-16 17:23:47 -0300

    desenvolvimento data 4

---
+++ data/resposta_chamado/resposta_chamado_model.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from data.admin.admin_model import Admin
+
+
+@dataclass
+class resposta_chamado:
+    Id: int
+    feedback: str
+    dataEnvio: str
+    visualizacao: bool
+    horaEnvio: str
+    IdAdmin: Admin[0]
