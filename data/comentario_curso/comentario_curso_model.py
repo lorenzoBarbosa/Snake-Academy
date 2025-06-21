6127f43ab2f17dc8ef7b189b6839c297700b1403
MateusCuman <mateuscumansantos@gmail.com>
2025-06-21 13:38:50 -0300

    desenvolvimento data 12

---
+++ data/comentario_curso/comentario_curso_model.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from data.admin.admin_model import Admin
+from data.matricula.matricula_model import matricula
+
+@dataclass
+
+class comentarioCurso:
+    id: int
+    idAdmin: Admin
+    idMatricula: matricula
+    conteudo: str
+    dataEnvio: str
+    dataSupervisaoAdmin: str
+    visualizacao: bool
+
+#não sei se está certo Lorenzo
