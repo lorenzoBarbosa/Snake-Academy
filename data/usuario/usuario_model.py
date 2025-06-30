767cea66bd03876a8f92583d8ea69c6b9d49a186
MateusCuman <mateuscumansantos@gmail.com>
2025-06-30 18:16:47 -0300

    testes de chamado finalizados

--- data/usuario/usuario_model.py
+++ data/usuario/usuario_model.py
@@ -9,3 +9,14 @@ class Usuario:
     senha: str
     telefone: str
     dataCriacao: str
+
+    @classmethod  #criei esse metódo para simplifcar em chamado_repo, no caso para eu pegar só o id
+    def somente_id(cls, id: int):
+        return cls(
+            id=id,
+            nome="",
+            email="",
+            senha="",
+            telefone="",
+            dataCriacao=""
+        )
