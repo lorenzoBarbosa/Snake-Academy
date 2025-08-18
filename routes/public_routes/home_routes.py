f60d3d66144de59589eae115b740eddcf363aec8
lorenzoBarbosa <lorenzobarbosaalvea@gmail.com>
2025-08-18 16:55:50 -0300

    bowliiiiing

--- routes/public_routes/home_routes.py
+++ routes/public_routes/home_routes.py
@@ -1,14 +1,32 @@
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 
-from data.usuario import usuario_repo
+from data.admin.admin_repo import *
+from data.banner.banner_repo import *
+from data.usuario.usuario_repo import *
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
+criar_tabela_usuario()
+criar_tabela_admin()
+criar_tabela_banner()
+
+for u in range(3):
+    usuario = Usuario(id=0, nome=f"Usuário {u}", email=f"usuario{u}@exemplo.com", senha="senha123", telefone="123456789", dataCriacao='2023-01-01')
+    id = inserir_usuario(usuario)
+
+id_admin = inserir_admin(id=usuario.id, admin= Admin(id=usuario.id, nome= usuario.nome, email=usuario.email, senha=usuario.senha, telefone=usuario.telefone, dataCriacao=usuario.dataCriacao, nivelAcesso=1))
+
+for i in range(3):
+    inserir_banner(Banner(id=0, idAdmin=id_admin, status="ativo"))
+
+
+
 @router.get("/")
 async def get_root():
-    response = templates.TemplateResponse("publico/home.html", {"request": {}})
+    banners = obter_todos_banners()
+    response = templates.TemplateResponse("publico/home.html", {"request": {}, "banners": banners})
     return response
 
 
