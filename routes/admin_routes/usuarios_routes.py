84de1fb344f82057c854660f62fe0844f7c22b0a
JhonatanLimaMotaLourenco <jhonatanlmlourenco@gmail.com>
2025-08-18 18:09:51 -0300

    Começo da criação dos htmls e rotas de administrador

---
+++ routes/admin_routes/usuarios_routes.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/usuarios")
+async def get_usuarios():
+    response = templates.TemplateResponse("admin/usuarios.html", {"request": {}})
+    return response
