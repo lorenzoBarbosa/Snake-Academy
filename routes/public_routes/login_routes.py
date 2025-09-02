5f4529613e6ee53392a6d07275df37ebdeaab538
lorenzoBarbosa <lorenzobarbosaalvea@gmail.com>
2025-09-02 14:33:13 -0300

    rells do tik tok

--- routes/public_routes/login_routes.py
+++ routes/public_routes/login_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.params import Form
 
@@ -15,8 +16,9 @@ async def get_login():
 @router.post("/login")
 async def post_login(email: str = Form(...), senha: str = Form(...)):
     usuario = usuario_repo.obter_usuario_por_email(email)
+    id = usuario.id if usuario else None
     if usuario and usuario.senha == senha:
-        response = templates.TemplateResponse("publico/cliente.html", {"request": {}, "usuario": usuario})
+        return RedirectResponse(url=f"/cliente{id}", status_code=303)
     else:
         response = templates.TemplateResponse("publico/login.html", {"request": {}, "mensagem": "Email ou senha inválidos."})
     return response
