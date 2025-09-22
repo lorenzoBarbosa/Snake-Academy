aadc3ea1968f9044a5ec55d2bf485b80e7bdba91
lorenzoBarbosa <lorenzobarbosaalvea@gmail.com>
2025-09-22 17:49:29 -0300

    a gente ta subindo neeee?

--- routes/cliente_routes/carrinho_routes.py
+++ routes/cliente_routes/carrinho_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 from util.auth_decorator import requer_autenticacao
@@ -8,8 +8,8 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/cliente/carrinho")
 @requer_autenticacao(["cliente", 'professor', 'admin'])
-async def get_carrinho():
-    response = templates.TemplateResponse("cliente/carrinho.html", {"request": {}})
+async def get_carrinho(request: Request, usuario_logado: dict = None):
+    response = templates.TemplateResponse("cliente/carrinho.html", {"request": request, "usuario": usuario_logado})
     return response
 
 
