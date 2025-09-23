2e1d1e9dc4605c0833dfba964e1686e818242ca0
lorenzoBarbosa <lorenzbarbosaalvea@gmail.com>
2025-09-22 22:15:35 -0300

    mais um cadiquin e já é terceiro lugar em

--- routes/admin_routes/administrador_routes.py
+++ routes/admin_routes/administrador_routes.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
+from data.banner.banner_repo import *
 from util.auth_decorator import requer_autenticacao
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
+banners = obter_todos_banners()
+
 @router.get("/administrador")
 @requer_autenticacao(["admin"])
 async def get_administrador(request:Request, usuario_logado: dict = None):
-    response = templates.TemplateResponse("administrador/administrador.html", {"request": request, "usuario": usuario_logado})
+    response = templates.TemplateResponse("administrador/administrador.html", {"request": request, "usuario": usuario_logado, "banners": banners})
     return response
--- routes/admin_routes/banners_routes.py
+++ routes/admin_routes/banners_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 from util.auth_decorator import requer_autenticacao
@@ -8,6 +8,6 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/admin/banners")
 @requer_autenticacao(["admin"])
-async def get_banners():
-    response = templates.TemplateResponse("admin/banners/banners.html", {"request": {}})
+async def get_banners(request:Request, usuario_logado: dict = None):
+    response = templates.TemplateResponse("administrador/banners/banners.html", {"request": request, "usuario": usuario_logado})
     return response
