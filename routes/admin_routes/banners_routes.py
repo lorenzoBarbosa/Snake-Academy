79e737c536d7b19c3ee7b49c8cf4af674d2db981
lorenzoBarbosa <lorenzobarbosaalvea@gmail.com>
2025-09-29 18:05:59 -0300

    tão falando que é o litle hair diz aí quem não sabe né, diz aí quem não sabe,

--- routes/admin_routes/banners_routes.py
+++ routes/admin_routes/banners_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
+from data.banner.banner_repo import *
 from util.auth_decorator import requer_autenticacao
 
 router = APIRouter()
@@ -9,5 +10,6 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/admin/banners")
 @requer_autenticacao(["admin"])
 async def get_banners(request:Request, usuario_logado: dict = None):
-    response = templates.TemplateResponse("admin/banners/banners.html", {"request": request, "usuario": usuario_logado})
+    banners = obter_todos_banners()
+    response = templates.TemplateResponse("admin/banners/banners.html", {"request": request, "usuario": usuario_logado, "banners": banners})
     return response
