06d96f657248e7358c750d694fd7ea46c7611fd6
lorenzoBarbosa <lorenzobarbosaalvea@gmail.com>
2025-10-01 18:01:51 -0300

    mas que coisa em

--- routes/admin_routes/alterar_categoria_routes.py
+++ routes/admin_routes/alterar_categoria_routes.py
@@ -7,10 +7,11 @@ from util.auth_decorator import requer_autenticacao
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/admin/alterar-categoria/{id}")
+@router.get("/admin/categorias/alterar-categoria/{id}")
 @requer_autenticacao(["admin"])
-async def get_alterar_categoria(request: Request, id: int, usuario_logado: dict = None):
+async def get_alterar_categoria(request: Request, id: int = None, usuario_logado: dict = None):
     categorias = categoria_repo.obter_categorias()
-    response = templates.TemplateResponse("admin/categorias/alterar_categoria.html", {"request": request, "usuario": usuario_logado})
+    categoria = categoria_repo.obter_categoria_por_id(id)
+    response = templates.TemplateResponse("admin/categorias/alterar_categoria.html", {"request": request, "usuario": usuario_logado, "categorias": categorias, "categoria": categoria})
     return response
 
--- routes/admin_routes/inserir_categoria_routes.py
+++ routes/admin_routes/inserir_categoria_routes.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Request
+from fastapi.params import Form
 from fastapi.templating import Jinja2Templates
 
+from data.categoria import categoria_repo
+from data.categoria.categoria_model import Categoria
 from util.auth_decorator import requer_autenticacao
 
 router = APIRouter()
@@ -11,3 +14,14 @@ templates = Jinja2Templates(directory="templates")
 async def get_inserir_categoria(request: Request, usuario_logado: dict = None):
     response = templates.TemplateResponse("admin/categorias/inserir_categoria.html", {"request": request, "usuario": usuario_logado})
     return response
+
+@router.post("/admin/categorias/inserir-categoria")
+@requer_autenticacao(["admin"])
+async def post_inserir_categoria(request: Request,
+                                  usuario_logado: dict = None,
+                                  nome: str = Form(...)):
+    categoria = Categoria(id=0, nome=nome)
+    try:
+        categoria_repo.inserir_categoria(categoria)
+    except Exception as e:
+        return templates.TemplateResponse("admin/categorias/inserir_categoria.html", {"request": request, "erro": "Algo deu errado, tente novamente.", "usuario": usuario_logado})
