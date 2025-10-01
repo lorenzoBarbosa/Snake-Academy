fc7eb3fce010154920c0e67939d3ac6895779940
Jhonatan-jobs <jhonatanlmlourenco@gmail.com>
2025-10-01 17:40:36 -0300

    estão gozando com a minha p#@$

--- routes/admin_routes/alterar_categoria_routes.py
+++ routes/admin_routes/alterar_categoria_routes.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
+from data.categoria import categoria_repo
 from util.auth_decorator import requer_autenticacao
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/admin/alterar-categoria")
+@router.get("/admin/alterar-categoria/{id}")
 @requer_autenticacao(["admin"])
-async def get_alterar_categoria(request: Request, usuario_logado: dict = None):
+async def get_alterar_categoria(request: Request, id: int, usuario_logado: dict = None):
+    categorias = categoria_repo.obter_categorias()
     response = templates.TemplateResponse("admin/categorias/alterar_categoria.html", {"request": request, "usuario": usuario_logado})
     return response
+
